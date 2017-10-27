b5a55888a7d91e43bb70b95b285c623bc7feb563
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-10-27 15:33:49 +0800

    Add dragging for mouseup effect for straightline.

--- scripts/drawing-straight-line.js
+++ scripts/drawing-straight-line.js
@@ -13,9 +13,11 @@ class DrawingStraightLine extends PaintFunction{
         this.drawStraightLine(this.contextDraft,coord[0],coord[1],this.origX, this.origY);
     }
     onMouseMove(){}
-    onMouseUp(coord){
+    onMouseUp(coord,event,dragging){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.drawStraightLine(this.contextReal,coord[0],coord[1],this.origX, this.origY);
+        if(dragging){
+            this.drawStraightLine(this.contextReal,coord[0],coord[1],this.origX, this.origY);
+        }
     }
     onMouseLeave(){}
     onMouseEnter(){}
