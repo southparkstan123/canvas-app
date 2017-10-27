e092aac7db4bc1fc24f90888fd9b8b9c04ed82c6
Isaac Yuen <whyuenac@gmail.com>
2017-10-27 16:54:06 +0800

    eraser added

---
+++ scripts/eraser.js
@@ -0,0 +1,40 @@
+class Eraser extends PaintFunction {
+    constructor(contextReal) {
+        super();
+        this.context = contextReal;
+    }
+
+    onMouseDown(coord, event) {
+        var backgroundcolor = $('#canvas-real').css("backgroundColor")
+        
+        this.context.strokeStyle = backgroundcolor;
+
+        console.log(this.context.strokeStyle);
+
+        this.context.lineWidth=5;
+        this.context.beginPath();
+        this.context.moveTo(coord[0], coord[1]);
+        this.draw(coord[0], coord[1]);
+    }
+    onDragging(coord, event) {
+        this.draw(coord[0], coord[1]);
+    }
+
+    onMouseMove() { }
+    onMouseUp() { }
+    onMouseLeave(coord, event, dragging) {
+        if (dragging) {
+            this.context.beginPath();
+            this.context.moveTo(coord[0], coord[1]);
+            this.draw(coord[0], coord[1]);
+        }
+    }
+    onMouseEnter() { }
+
+    draw(x, y) {
+        this.context.lineTo(x, y);
+        this.context.moveTo(x, y);
+        this.context.closePath();
+        this.context.stroke();
+    }
+}
