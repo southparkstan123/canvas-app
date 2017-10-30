80f5591e1109e0b32bae3f615af38bf828848258
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-10-30 15:26:21 +0800

    Upload the image on canvas

---
+++ scripts/upload-image.js
@@ -0,0 +1,15 @@
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = new Image();
+           img.addEventListener("load", function() {
+             contextDraft.drawImage(img, 0, 0);
+           });
+           img.src = e.target.result;
+        };       
+        FR.readAsDataURL( this.files[0] );
+    }
+}
+
+document.getElementById("fileUpload").addEventListener('change',readImage, false);
