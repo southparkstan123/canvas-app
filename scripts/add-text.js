f9906d015e7db9e95f9aced49c6f52c691f6d8cf
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-10-31 17:02:43 +0800

    Add onchange event for font style

--- scripts/add-text.js
+++ scripts/add-text.js
@@ -1,40 +1,128 @@
 class AddText extends PaintFunction{
-    constructor(contextReal){
+    constructor(contextReal, contextDraft){
         super();
         this.contextReal = contextReal;
+        this.contextDraft = contextDraft;
     }
 
-    addText(context,text,fillOrStroke,fontFamily,size,style,xpos,ypos){
+    addText(context, data, isDraft){
+        console.log(data);
+        
+        var xpos = localStorage.getItem('x');
+        var ypos = localStorage.getItem('y');
+
+        context.textBaseline = 'top';
         context.strokeStyle = this.strokeColor;
         context.fillStyle=this.strokeColor;
-        switch(style) {
+
+        // Clear the draft context only
+        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+
+        switch(data.style) {
             case 'normal':
-                context.font = `normal normal ${size}px ${fontFamily}`;
+                context.font = `normal normal ${data.size}px ${data.fontFamily}`;
                 break;
             case 'bold':
-                context.font = `normal bold ${size}px ${fontFamily}`;
+                context.font = `normal bold ${data.size}px ${data.fontFamily}`;
                 break;
             case 'italic':
-                context.font = `italic normal ${size}px ${fontFamily}`;
+                context.font = `italic normal ${data.size}px ${data.fontFamily}`;
                 break;
             case 'both':
-                context.font = `italic bold ${size}px ${fontFamily}`;
+                context.font = `italic bold ${data.size}px ${data.fontFamily}`;
+                break;
+            default:
+                context.font = `normal normal ${data.size}px ${data.fontFamily}`;
                 break;
         }
-        
-        context.textBaseline = 'top';
-        console.log(context.font);
-        if(fillOrStroke === 'stroke'){          
-            context.strokeText(text,xpos,ypos);
-        }else if(fillOrStroke === 'fill'){
-            context.fillText(text,xpos,ypos);
+
+        if(data.text === null){
+            data.text = "Your Text";
         }
+        
+        if(data.fillOrStroke === 'stroke'){          
+            context.strokeText(data.text,xpos,ypos);
+        }else{
+            context.fillText(data.text,xpos,ypos);
+        }
+
+        if(!isDraft){
+            this.removeAllTextData();
+        }
+
+    }
+    onMouseDown(coord,event) {
+        let textarea = null;
+        this.canvas = document.getElementById("canvas-draft");
+
+        localStorage.setItem('x',event.clientX - this.canvas.offsetLeft);
+        localStorage.setItem('y',event.clientY - this.canvas.offsetTop);
+
+        $('#xpos').text(localStorage.getItem('x'));
+        $('#ypos').text(localStorage.getItem('y'));
+
+        //Reset
+        this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+        this.addText(this.contextDraft,this.textDataHandler(),true);
+
     }
 
-    onMouseDown() { }
-    onDragging() { }
+    textDataHandler(){
+        let data = {};
+        data = {
+            'text' : localStorage.getItem('text'),
+            'size' : localStorage.getItem('size'),
+            'fillOrStroke' : localStorage.getItem('fillOrStroke'),
+            'fontFamily' : localStorage.getItem('fontFamily'),
+            'style' : localStorage.getItem('style'),
+        }
+        return data;
+    }
+    removeAllTextData(){
+        localStorage.removeItem('text');
+        localStorage.removeItem('size');
+        localStorage.removeItem('fillOrStroke');
+        localStorage.removeItem('fontFamily');
+        localStorage.removeItem('style');
+        localStorage.removeItem('x');
+        localStorage.removeItem('y');
+    }
+    onChangeText(context, value){
+        localStorage.setItem('text',value);
+        this.addText(context, this.textDataHandler(),true);
+    }
+    onChangeSize(context, value){
+        localStorage.setItem('size',value);
+        this.addText(context, this.textDataHandler(),true);
+    }
+    onChangeSize(context, value){
+        localStorage.setItem('size',value);
+        this.addText(context, this.textDataHandler(),true);
+    }
+    onChangeFontFamily(context, value){
+        localStorage.setItem('fontFamily',value);
+        this.addText(context, this.textDataHandler(),true);
+    }
+    onChangeStyle(context, value){
+        localStorage.setItem('style',value);
+        this.addText(context, this.textDataHandler(),true);
+    }
+    onChangeFillOrStroke(context, value){
+        localStorage.setItem('fillOrStroke',value);
+        this.addText(context, this.textDataHandler(),true);
+    }
+    onSubmit(){
+        this.addText(this.contextReal,this.textDataHandler(),false);
+    }
+    onInit(){
+
+    }
+
+    onDragging() {}
     onMouseMove() { }
-    onMouseUp() { }
+    onMouseUp() {}
+
+
     onMouseLeave() { }
     onMouseEnter() { }
 }
