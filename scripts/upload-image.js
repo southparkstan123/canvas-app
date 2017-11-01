693bfe86b81f8afa98fa5a07505ee86d9731ad1f
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-11-01 14:39:00 +0800

    Fix the background problem when upload the canvas

---
+++ scripts/upload-image.js
@@ -0,0 +1,34 @@
+class UploadImage extends PaintFunction{
+    constructor(canvas) {
+        super();
+        this.canvas = canvas;
+    }
+
+    uploadImage(){
+
+        let context = this.canvas.getContext('2d');
+        let originalColor = context.fillStyle;//Store the original fill style
+
+        context.globalCompositeOperation = 'destination-over';
+
+        context.fillStyle = $("#background").spectrum('get').toHexString();//Set the color as the same as the canvas which is set in CSS
+
+        context.fillRect(0,0,this.canvas.width,this.canvas.height);
+
+        this.canvas.toBlob(function(blob) {
+            let d = new Date();
+            let timestamp = d.getTime();
+            let filename = 'canvas' + timestamp;
+
+            let storageRef = firebase.storage().ref();
+
+            let canvasRef = storageRef.child(filename);
+            canvasRef.put(blob).then(function(snapshot) {
+                console.log(filename + 'is uploaded!');
+            });
+        });
+        
+        context.globalCompositeOperation = 'source-over';//Set default type of composite operation
+        context.fillStyle = originalColor;//Set the color as the same as the original color
+    }
+}
